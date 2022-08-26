13cf92b7ddde1309cd4a3a526e227b6908e43dd8
HTSGE <110009644+HTSGE@users.noreply.github.com>
2022-08-26 09:40:43 +0800

    Add files via upload

---
+++ data.source.js
@@ -0,0 +1,479 @@
+import React from 'react';
+export const Nav30DataSource = {
+  wrapper: { className: 'header3 home-page-wrapper jzih1dpqqrg-editor_css' },
+  page: { className: 'home-page' },
+  logo: { className: 'header3-logo jzjgnya1gmn-editor_css', children: '' },
+  Menu: {
+    className: 'header3-menu',
+    children: [
+      {
+        name: 'item1',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  <p>
+                    <br />
+                  </p>
+                </span>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+        subItem: [
+          {
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item jzj8295azrs-editor_css',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image jzj81c9wabh-editor_css',
+                  children:
+                    'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*4_S6ToPfj-QAAAAAAAAAAABkARQnAQ',
+                },
+              ],
+            },
+            name: 'sub~jzj8hceysgj',
+          },
+        ],
+      },
+      {
+        name: 'item2',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  <p>
+                    <br />
+                  </p>
+                </span>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header3-mobile-menu' },
+};
+export const Banner50DataSource = {
+  wrapper: { className: 'home-page-wrapper banner5' },
+  page: { className: 'home-page banner5-page' },
+  childWrapper: {
+    className: 'banner5-title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>欧尔工艺</p>
+          </span>
+        ),
+        className: 'banner5-title',
+      },
+      {
+        name: 'explain',
+        className: 'banner5-explain',
+        children: (
+          <span>
+            <span>
+              <span>
+                <p>一个开发者团队</p>
+              </span>
+            </span>
+          </span>
+        ),
+      },
+      {
+        name: 'content',
+        className: 'banner5-content',
+        children: (
+          <span>
+            <span>
+              <p>带给开发者一个即时创作的平台</p>
+            </span>
+          </span>
+        ),
+      },
+      {
+        name: 'button',
+        className: 'banner5-button-wrapper',
+        children: {
+          href: '#',
+          className: 'banner5-button',
+          type: 'primary',
+          children: (
+            <span>
+              <p>查看详情</p>
+            </span>
+          ),
+        },
+      },
+    ],
+  },
+  image: {
+    className: 'banner5-image',
+    children:
+      'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*-wAhRYnWQscAAAAAAAAAAABkARQnAQ',
+  },
+};
+export const Feature10DataSource = {
+  wrapper: { className: 'home-page-wrapper content1-wrapper' },
+  OverPack: { className: 'home-page content1', playScale: 0.3 },
+  imgWrapper: { className: 'content1-img', md: 10, xs: 24 },
+  img: {
+    children: 'https://zos.alipayobjects.com/rmsportal/nLzbeGQLPyBJoli.png',
+  },
+  textWrapper: { className: 'content1-text', md: 14, xs: 24 },
+  title: {
+    className: 'content1-title',
+    children: (
+      <span>
+        <span>
+          <span>
+            <span>
+              <p>游戏组件开发</p>
+            </span>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+  content: {
+    className: 'content1-content',
+    children: (
+      <span>
+        <span>
+          <span>
+            <p>
+              欧尔工艺旗下拥有许多优秀的游戏组件开发者，其中《我的世界》中国版开发者较多。旗下优秀作品有：《冬阳西台市》、
+            </p>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+};
+export const Feature40DataSource = {
+  wrapper: { className: 'home-page-wrapper content6-wrapper' },
+  OverPack: { className: 'home-page content6' },
+  textWrapper: { className: 'content6-text', xs: 24, md: 10 },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <p>助您一臂之力！</p>
+            </span>
+          </span>
+        ),
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        className: 'title-content',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <p>欧尔工艺提供专业的服务支持</p>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+      },
+    ],
+  },
+  img: {
+    children: 'https://zos.alipayobjects.com/rmsportal/xBrUaDROgtFBRRL.png',
+    className: 'content6-img',
+    xs: 24,
+    md: 14,
+  },
+  block: {
+    children: [
+      {
+        name: 'block0',
+        img: {
+          children:
+            'https://zos.alipayobjects.com/rmsportal/NKBELAOuuKbofDD.png',
+          className: 'content6-icon',
+        },
+        title: { className: 'content6-title', children: '技术' },
+        content: {
+          className: 'content6-content',
+          children: (
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <span>
+                      <p>
+                        团队旗下开发者拥有强大的建筑技术与指令技术，在评分未低于4.0的作品中显而易见。
+                      </p>
+                    </span>
+                  </span>
+                </span>
+              </span>
+            </span>
+          ),
+        },
+      },
+      {
+        name: 'block1',
+        img: {
+          className: 'content6-icon',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/xMSBjgxBhKfyMWX.png',
+        },
+        title: {
+          className: 'content6-title',
+          children: (
+            <span>
+              <p>宣传</p>
+            </span>
+          ),
+        },
+        content: {
+          className: 'content6-content',
+          children: (
+            <span>
+              <p>
+                团队旗下运营有全网1.2w粉丝账号：“青稚”自媒体账号、欧尔工艺公众号等
+              </p>
+            </span>
+          ),
+        },
+      },
+      {
+        name: 'block2',
+        img: {
+          className: 'content6-icon',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/MNdlBNhmDBLuzqp.png',
+        },
+        title: {
+          className: 'content6-title',
+          children: (
+            <span>
+              <p>开发</p>
+            </span>
+          ),
+        },
+        content: {
+          className: 'content6-content',
+          children: (
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <p>团队旗下强大的技术可助您在服务器开发中一臂之力！</p>
+                  </span>
+                </span>
+              </span>
+            </span>
+          ),
+        },
+      },
+    ],
+  },
+};
+export const Footer10DataSource = {
+  wrapper: { className: 'home-page-wrapper footer1-wrapper' },
+  OverPack: { className: 'footer1', playScale: 0.2 },
+  block: {
+    className: 'home-page',
+    gutter: 0,
+    children: [
+      {
+        name: 'block0',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          className: 'logo',
+          children: (
+            <span>
+              <p>OER</p>
+            </span>
+          ),
+        },
+        childWrapper: {
+          className: 'slogan',
+          children: [
+            {
+              name: 'content0',
+              children: (
+                <span>
+                  <p>欧尔工艺官网</p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          children: (
+            <span>
+              <p>成员</p>
+            </span>
+          ),
+        },
+        childWrapper: {
+          children: [
+            {
+              name: 'link0',
+              href: 'https://v.douyin.com/jMVypvx/',
+              children: (
+                <span>
+                  <span>
+                    <p>青稚</p>
+                  </span>
+                </span>
+              ),
+            },
+            {
+              name: 'link1',
+              href: '#',
+              children: (
+                <span>
+                  <p>卡尔-路德维希大公</p>
+                </span>
+              ),
+            },
+            {
+              name: 'link2',
+              href: 'https://v.douyin.com/jMVT1MY/',
+              children: (
+                <span>
+                  <span>
+                    <p>冬阳西台</p>
+                  </span>
+                </span>
+              ),
+              target: '_blank',
+            },
+            {
+              name: 'link3',
+              href: 'https://v.douyin.com/jMVbefX/',
+              children: (
+                <span>
+                  <p>Dream's Brother</p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          children: (
+            <span>
+              <p>我们</p>
+            </span>
+          ),
+        },
+        childWrapper: {
+          children: [
+            {
+              href: 'https://v.douyin.com/jMquUVX/',
+              name: 'link1',
+              children: (
+                <span>
+                  <span>
+                    <p>抖音号</p>
+                  </span>
+                </span>
+              ),
+              target: '_blank',
+            },
+            {
+              href: 'http://weixin.qq.com/r/bBIIEKbEx-u4reJz90d_',
+              name: 'link0',
+              children: (
+                <span>
+                  <p>公众号</p>
+                </span>
+              ),
+              target: '_blank',
+            },
+          ],
+        },
+      },
+      {
+        name: 'block3',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          children: (
+            <span>
+              <p>友情链接</p>
+            </span>
+          ),
+        },
+        childWrapper: {
+          children: [
+            {
+              href: '',
+              name: 'link0',
+              children: (
+                <span>
+                  <p>创客与创造QQ频道</p>
+                </span>
+              ),
+            },
+            {
+              href: '#',
+              name: 'link1',
+              children: (
+                <span>
+                  <p>MC资讯网</p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+    ],
+  },
+  copyrightWrapper: { className: 'copyright-wrapper' },
+  copyrightPage: { className: 'home-page' },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        <span>
+          <span>
+            <span>
+              ©2022 by <b>OER</b>&nbsp;All Rights Reserved
+            </span>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+};
